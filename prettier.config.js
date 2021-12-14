8086ce0881e3a10f28f6bcafde073a0b91b22cff
jorgezepeda <baattts@gmail.com>
2021-12-13 18:00:29 -0800

    commiting config files

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const options = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    endOfLineL: 'lf',
+    semi: false,
+    tabWidth: 4,
+    trailingComma: 'none'
+}
+
+module.exports = options
